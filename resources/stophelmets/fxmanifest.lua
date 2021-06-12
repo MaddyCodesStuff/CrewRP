801ec82bb9f1b876d52ff2a8816f38a2222f3930
kunaiknife452 <kunaiknife452@gmail.com>
2021-06-12 04:09:30 -0400

    final touches

---
+++ resources/stophelmets/fxmanifest.lua
@@ -0,0 +1,12 @@
+-- Resource Metadata
+fx_version 'cerulean'
+game 'gta5'
+
+author 'TheCrewRp'
+description 'Stops Player from putting on generic helmets'
+version '1.0.0'
+
+-- What to run
+client_scripts {
+    'client.lua',
+}
