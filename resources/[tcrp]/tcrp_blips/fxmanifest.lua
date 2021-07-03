010e8f0f874c834a0d89651b65df75aa87dd1d90
kunaiknife452 <kunaiknife452@gmail.com>
2021-07-03 00:29:56 -0400

    fxmanifest

---
+++ resources/[tcrp]/tcrp_blips/fxmanifest.lua
@@ -0,0 +1,12 @@
+-- Resource Metadata
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Kunai'
+description 'Handles All Blips'
+version '1.0.0'
+
+-- What to run
+client_scripts {
+    'client.lua',
+}
