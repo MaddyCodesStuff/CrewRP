854f247f2af2ada4507c28dbf97d152f79cd0bc4
wizard-bailey <82252694+wizard-bailey@users.noreply.github.com>
2021-07-05 03:17:09 -0500

    Fixed SQL

--- resources/[tcrp]/tcrp-mining/mining.sql
+++ resources/[tcrp]/tcrp-mining/mining.sql
@@ -1,7 +1,7 @@
 USE `essentialmode`;
 
-INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`, `weight`)
-VALUES ('jackhammer', 'Jackhammer', 1, 0, 1, 1);
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`)
+VALUES ('jackhammer', 'Jackhammer', 1, 0, 1);
 UPDATE `items`
 SET `label` = 'Mineral Ore'
 WHERE `name` = 'washed_stone';
