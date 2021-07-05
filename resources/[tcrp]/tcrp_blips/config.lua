0cba4599aa5186af7a2b668295133c8535ce89b5
kunaiknife452 <kunaiknife452@gmail.com>
2021-07-05 00:45:20 -0400

    test

--- resources/[tcrp]/tcrp_blips/config.lua
+++ resources/[tcrp]/tcrp_blips/config.lua
@@ -1,7 +1,12 @@
 DefaultSprite       = 1,
 DefaultSpriteColor  = 0,
-DefaultScale        = 1.0,
+DefaultScale        = 0.8,
 DefaultSpriteName   = "New Blip",
 DefaultType         = 1
-DefaultDetail       = true
 DefaultTransparency = 100
+
+[1]  = {
+["blip"]       = {
+        ["x"] = 29.41, ["y"] = -1345.01, ["z"] = 29.5
+    },
+}, -- Done
