a25e7a41db9dab6670f03d836e0cfbee38a4e0e5
Kaia <kaia@kaia.dev>
2021-07-15 12:45:58 -0500

    Update main.lua
    DELETE YOUR FUCKING COMMENTS

--- resources/[esx]/esx_carwash/client/main.lua
+++ resources/[esx]/esx_carwash/client/main.lua
@@ -48,9 +48,7 @@ Citizen.CreateThread(function()
                 local carWashLocation = Config.Locations[i]
                 local distance        = GetDistanceBetweenCoords(coords, carWashLocation, true)
 
-                if distance < 50 then
-                    --DrawMarker(1, carWashLocation, 0, 0, 0, 0, 0, 0, 5.0, 5.0, 2.0, 0, 157, 0, 155, false, false, 2,
-                               --false, false, false, false)
+                if distance < 50 then        
                     canSleep = false
                 end
 
