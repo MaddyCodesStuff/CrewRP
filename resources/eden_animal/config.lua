8dc65c2933a271ac4f8263c72448b629589fdae7
wizard-bailey <82252694+wizard-bailey@users.noreply.github.com>
2021-07-29 23:31:22 -0500

    PETSSSSS

---
+++ resources/eden_animal/config.lua
@@ -0,0 +1,77 @@
+Config = {}
+
+Config.Locale = 'en'
+
+Config.PetShop = {
+	{
+		pet = 'chien',
+		label = _U('rottweiler'),
+		price = 5
+	},
+
+	{
+		pet = 'chat',
+		label = _U('cat'),
+		price = 5
+	},
+
+	{
+		pet = 'lapin',
+		label = _U('bunny'),
+		price = 5
+	},
+
+	{
+		pet = 'husky',
+		label = _U('husky'),
+		price = 5
+	},
+
+	{
+		pet = 'cochon',
+		label = _U('pig'),
+		price = 5
+	},
+
+	{
+		pet = 'caniche',
+		label = _U('poodle'),
+		price = 5
+	},
+
+	{
+		pet = 'carlin',
+		label = _U('pug'),
+		price = 5
+	},
+
+	{
+		pet = 'retriever',
+		label = _U('retriever'),
+		price = 5
+	},
+
+	{
+		pet = 'berger',
+		label = _U('asatian'),
+		price = 5
+	},
+
+	{
+		pet = 'westie',
+		label = _U('westie'),
+		price = 5
+	},
+}
+
+Config.Zones = {
+	PetShop = {
+		Pos = {x = 562.19, y = 2741.30, z = 41.86 },
+		Sprite = 463,
+		Display = 4,
+		Scale = 1.0,
+		Size  = {x = 1.5, y = 1.5, z = 1.0},
+		Color = {r = 204, g = 204, b = 0},
+		Type  = 1
+	}
+}
