833358e4ed7931353e9a4b0395ce6785ce663cb4
kunaiknife452 <kunaiknife452@gmail.com>
2021-08-03 04:47:02 -0400

    thats not supposed to be there

--- resources/[tcrp]/tcrp-wheelchair/config.lua
+++
@@ -1,5 +0,0 @@
-Config = {
-    MaxSpeedKmh = 10,
-    maxJumpHeigh = 0.0,
-    LoseConnectionDistance = 100.0
-}
--- resources/[tcrp]/tcrp-wheelchair/database items.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('wheelchair', 'Wheelchair', 1, 0, 1);
