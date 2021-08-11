a17ec70b0c299d77633fa4ff579d39c7bff6772f
kunaiknife452 <kunaiknife452@gmail.com>
2021-08-10 20:23:17 -0400

    fxmanifest

--- resources/[tcrp]/tcrp-weaponry/fxmanifest.lua
+++ resources/[tcrp]/tcrp-weaponry/fxmanifest.lua
@@ -1,6 +1,9 @@
 fx_version 'cerulean'
 games {'gta5'}
 
+author 'Kunai'
+version '1.0'
+description 'Completely rewritten fork of Weaponry -- Lyrad/Blumlaut'
 client_scripts {
     'client.lua',
     'config.lua'
