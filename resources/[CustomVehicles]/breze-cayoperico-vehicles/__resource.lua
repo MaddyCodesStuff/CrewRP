1584c59b949390c4d354cd6c6b78d495c17937f8
tttony2x <tttony2x@gmail.com>
2021-08-27 13:28:39 -0400

    new vehicles, handling may change

---
+++ resources/[CustomVehicles]/breze-cayoperico-vehicles/__resource.lua
@@ -0,0 +1,66 @@
+resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
+
+files { 
+    --Vehicls
+'vehicles.meta', 
+'carvariations.meta', 
+'carcols.meta', 
+'handling.meta', 
+'vehiclelayouts.meta',
+'vehiclelayouts_avisa.meta',
+'vehiclelayouts_longfin.meta',
+'vehiclelayouts_patrolboat.meta',
+--Weapons
+'vehicleweapons_annihilator2.meta',
+'vehicleweapons_dinghy5.meta',
+'vehicleweapons_patrolboat.meta',
+'vehicleweapons_seasparrow2.meta',
+'vehicleweapons_submarine.meta',
+
+--Animations
+'conditionalanims.meta',
+'clip_sets.xml',
+
+--Sounds
+-- 'sfx/dlc_hei4/brioso2.awc',
+-- 'sfx/dlc_hei4/brioso2_npc.awc',
+-- 'sfx/dlc_hei4/italirsx.awc',
+-- 'sfx/dlc_hei4/italirsx_npc.awc',
+-- 'sfx/dlc_hei4/kosatka.awc',
+-- 'sfx/dlc_hei4/manchez2.awc',
+-- 'sfx/dlc_hei4/manchez2_npc.awc',
+-- 'sfx/dlc_hei4/slamtruck.awc',
+-- 'sfx/dlc_hei4/slamtruck_npc.awc',
+-- 'sfx/dlc_hei4/toreador.awc',
+-- 'sfx/dlc_hei4/toreador_npc.awc',
+-- 'sfx/dlc_hei4/verus.awc',
+-- 'sfx/dlc_hei4/verus_npc.awc',
+-- 'sfx/dlc_hei4/veto.awc',
+-- 'sfx/dlc_hei4/veto_npc.awc',
+-- 'sfx/dlc_hei4/weevil.awc',
+-- 'sfx/dlc_hei4/weevil_npc.awc',
+-- 'sfx/dlc_hei4/winky.awc',
+-- 'sfx/dlc_hei4/winky_npc.awc'
+-- 'dlchei4_game.dat151.rel',
+-- 'dlchei4_sounds.dat54.rel',
+} 
+
+data_file 'HANDLING_FILE' 'handling.meta' 
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta' 
+data_file 'CARCOLS_FILE' 'carcols.meta' 
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta' 
+data_file 'VEHICLE_LAYOUTS_FILE' 'vehiclelayouts.meta' 
+data_file 'VEHICLE_LAYOUTS_FILE' 'vehiclelayouts_avisa.meta' 
+data_file 'VEHICLE_LAYOUTS_FILE' 'vehiclelayouts_patrolboat.meta' 
+data_file 'VEHICLE_LAYOUTS_FILE' 'vehiclelayouts_longfin.meta' 
+data_file 'VEHICLE_LAYOUTS_FILE' 'vehicleweapons_annihilator2.meta' 
+data_file 'VEHICLE_LAYOUTS_FILE' 'vehicleweapons_dinghy5.meta' 
+data_file 'VEHICLE_LAYOUTS_FILE' 'vehicleweapons_patrolboat.meta' 
+data_file 'VEHICLE_LAYOUTS_FILE' 'vehicleweapons_seasparrow2.meta' 
+data_file 'VEHICLE_LAYOUTS_FILE' 'vehicleweapons_submarine.meta' 
+data_file 'CONDITIONAL_ANIMS_FILE' 'conditionalanims.meta'
+data_file 'CLIP_SETS_FILE' 'clip_sets.xml'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_hei4'
+-- data_file 'AUDIO_GAMEDATA' 'dlchei4_game.dat'
+-- data_file 'AUDIO_SOUNDDATA' 'dlchei4_sounds.dat'
+
