69b98c18f77551b646f5b9f8c9285730672180ed
Madison Tinsley <kaia@kaia.dev>
2021-09-13 19:26:48 -0500

    Forgot to take credit

--- resources/[tcrp]/tcrp-names/fxmanifest.lua
+++ resources/[tcrp]/tcrp-names/fxmanifest.lua
@@ -2,26 +2,6 @@
 fx_version 'cerulean'
 games { 'rdr3', 'gta5' }
 
-client_script 'playernames_api.lua'
-server_script 'playernames_api.lua'
+author 'Kaia'
 
 client_script 'playernames_cl.lua'
-server_script 'playernames_sv.lua'
-
--- make exports
-local exportList = {
-    'setComponentColor',
-    'setComponentAlpha',
-    'setComponentVisibility',
-    'setWantedLevel',
-    'setHealthBarColor',
-    'setNameTemplate'
-}
-
-exports(exportList)
-server_exports(exportList)
-
--- add files
-files {
-    'template/template.lua'
-}
