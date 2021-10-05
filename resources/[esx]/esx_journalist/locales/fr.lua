43adea739ae92c938b8668b2861964cfb0a503e5
wizard-bailey <82252694+wizard-bailey@users.noreply.github.com>
2021-10-05 00:00:10 -0500

    Added WeazelNews Interior and More

--- resources/[esx]/esx_journalist/locales/fr.lua
+++ resources/[esx]/esx_journalist/locales/fr.lua
@@ -1,41 +1,13 @@
-Locales['fr'] = {
-	-- Cloakroom
-	['cloakroom'] = 'Cloakroom',
-	['citizen_wear'] = 'Civilian Clothes',
-	['journalist_outfit'] = 'Trainee Outfit',
-	['journalist_outfit_1'] = 'Reporter Outfit',
-	['journalist_outfit_2'] = 'Investigator Outfit',
-	['journalist_outfit_3'] = 'Director Outfit', 
-    ['no_outfit'] = 'There is no uniform for your size...',
-    ['open_cloakroom'] = 'Press E to Change Yourself',
-	
-	-- Vehicles  
-    ['vehicle_menu']             = 'Vehicle',
-    ['vehicle_out']              = 'There is Already a Vehicle Outside',
-    ['vehicle_spawner']          = 'Press E to Select a Vehicle',
-    ['store_vehicle']            = 'Press E to Park the Vehicle',
-    ['service_max']              = 'Full Service : ',
-    ['spawn_point_busy']         = 'A Vehicle is Occupying the Spawn Point',
-	
+Locales['fr'] = {	
 	-- Boss Menu
 	['deposit_society'] = 'Deposit Money',
 	['withdraw_society'] = 'Withdraw Company Money',
 	['boss_actions'] = 'Boss Action',
 	-- Misc
 	['invalid_amount'] = 'Invalid Amount',
-	['open_menu'] = 'Press E to Open the Menu',
+	['open_menu'] = 'Press ~INPUT_CONTEXT~ to Open the Menu',
 	['deposit_amount'] = 'Deposit Amount',
 	['money_withdraw'] = 'Withdrawal Amount',
- 
-    -- Vault  
-    ['get_weapon']               = 'Take Weapon',
-    ['put_weapon']               = 'Deposit Weapon',
-    ['get_weapon_menu']          = 'Storage - Take Weapon',
-    ['put_weapon_menu']          = 'Storage - Deposit Weapon',
-    ['get_object']               = 'Take Object',
-    ['put_object']               = 'Deposit Object',
-    ['vault']                    = 'Storage',
-    ['open_vault']               = 'Press E to Access Storage',
 
     -- Boss Menu  
     ['take_company_money']       = 'Withdraw Company Money',
@@ -43,21 +15,11 @@ Locales['fr'] = {
     ['amount_of_withdrawal']     = 'Withdrawal Amount',
     ['invalid_amount']           = 'Amount Invalid',
     ['amount_of_deposit']        = 'Deposit Amount',
-    ['open_bossmenu']            = 'Press E to Open the Menu',
+    ['open_bossmenu']            = 'Press ~INPUT_CONTEXT~ to Open the Menu',
     ['invalid_quantity']         = 'Invalid Quantity',
     ['you_removed']              = 'You Have Withdrawn x',
     ['you_added']                = 'You Added x',
     ['quantity']                 = 'Quantity',
     ['inventory']                = 'Inventory',
-    ['unicorn_stock']            = 'Stock',
-
-    -- Misc 
-    ['map_blip']                 = 'Journalism Building',
-
-    -- Billing Menu  
-    ['billing']                  = 'Bill',
-    ['no_players_nearby']        = 'No Player Nearby',
-    ['billing_amount']           = 'Invoice Amount',
-    ['amount_invalid']           = 'Invalid Amount',
-    
+    ['unicorn_stock']            = 'Stock',  
 }
