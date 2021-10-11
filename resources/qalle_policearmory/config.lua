4b178e21564b7d6fe2e547d632d7d5fbcd68a785
kunaiknife452 <kunaiknife452@gmail.com>
2021-10-10 22:46:34 -0400

    final changes

--- resources/qalle_policearmory/config.lua
+++ resources/qalle_policearmory/config.lua
@@ -13,7 +13,7 @@ Config.ArmoryPed     = { ["x"] = 480.36, ["y"] = -996.62, ["z"] = 30.69, ["h"] =
 -- This is the available weapons you can pick out.
 Config.ArmoryWeapons = {
     {
-        ["hash"] = "weapon_taser",
+        ["hash"] = "weapon_taser2",
         ["type"] = "pistol"
     },
     {
