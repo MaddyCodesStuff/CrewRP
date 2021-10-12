364ebb3545522b2d02a03308f8f8ce6a6b26788a
EngineeringTuna <engineeringtuna@gmail.com>
2021-10-12 18:52:09 -0500

    I forgot to add the server main.

--- resources/[esx]/esx_ambulancejob/server/main.lua
+++ resources/[esx]/esx_ambulancejob/server/main.lua
@@ -147,26 +147,19 @@ end)
 RegisterServerEvent('esx_ambulancejob:giveItem')
 AddEventHandler('esx_ambulancejob:giveItem', function(itemName)
 	local xPlayer = ESX.GetPlayerFromId(source)
-	if xPlayer.job.name ~= 'ambulance' then
-		print(('esx_ambulancejob: %s attempted to spawn in an item!'):format(xPlayer.identifier))
-		return
 	elseif (xPlayer.job.grade_name ~= 'physician' and xPlayer.job.grade_name ~= 'sp' and xPlayer.job.grade_name ~= 'gp' and xPlayer.job.grade_name ~= 'studentsurgeon' and
 		xPlayer.job.grade_name ~= 'surgeon' and xPlayer.job.grade_name ~= 'chiefsurgery' and xPlayer.job.grade_name ~= 'cmo' and xPlayer.job.grade_name ~= 'boss') and
 		(itemName == 'morphine' or itemName == 'vicodin' or itemName == 'hydrocodone') then
 		TriggerClientEvent('esx:showNotification', source, 'You Do Not Have a Key for The Drug Locker')
-		print(('esx_ambulancejob: %s attempted to spawn in an item!'):format(xPlayer.identifier))
 		return
 	elseif (itemName ~= 'bodyarmor_3' and itemName ~= 'tylenol' and itemName ~= 'scuba' and itemName ~= 'medikit' and itemName ~= 'bandage' and itemName ~= 'gauze' and itemName ~= 'firstaid' and itemName ~= 'morphine' and itemName ~= 'vicodin' and itemName ~= 'hydrocodone' and itemName ~= 'medkit' and itemName ~= 'wheelchair') then
-		print(('esx_ambulancejob: %s attempted to spawn in an item!'):format(xPlayer.identifier))
 		return
 	end
 
 	local xItem = xPlayer.getInventoryItem(itemName)
 	local count = 1
 
-	print(itemName)
-
-	print(xItem.limit)
+	
 	if xItem.limit ~= -1 then
 		count = xItem.limit - xItem.count
 	end
@@ -182,7 +175,6 @@ end)
 TriggerEvent('es:addGroupCommand', 'revive', 'admin', function(source, args, user)
 	if args[1] ~= nil then
 		if GetPlayerName(tonumber(args[1])) ~= nil then
-			print(('esx_ambulancejob: %s used admin revive'):format(GetPlayerIdentifiers(source)[1]))
 			TriggerClientEvent('esx_ambulancejob:revive', tonumber(args[1]))
 		end
 	else
@@ -216,9 +208,6 @@ ESX.RegisterServerCallback('esx_ambulancejob:getDeathStatus', function(source, c
 	MySQL.Async.fetchScalar('SELECT is_dead FROM users WHERE identifier = @identifier', {
 		['@identifier'] = identifier
 	}, function(isDead)
-		if isDead then
-			print(('esx_ambulancejob: %s attempted combat logging!'):format(identifier))
-		end
 
 		cb(isDead)
 	end)
