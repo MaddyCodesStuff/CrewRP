68bf3e254e35d8d43583723c6729bd3e1472e2c9
tttony2x <tttony2x@gmail.com>
2021-10-21 20:27:40 -0400

    in with the newish

---
+++ resources/gcphone/locales/en.lua
@@ -0,0 +1,15 @@
+Locales['en'] = {
+    ["mission_row"] = "Mission Row Police",
+    ["phone_booth"] = "Phone Booth",
+    ["key_answer"] = "Press ~INPUT_PICKUP~ to answer the phone",
+    ["new_message"] = "New message",
+    ["new_message_from"] = "%s",
+    ["new_message_transmitter"] = "%s",
+    ["use_fixed"] = "%s's phone (%s) ~INPUT_PICKUP~ Use phone",
+    
+    -- Social
+    ["new_tweet"] = "New Tweet!",
+
+    -- Warning
+    ["no_phone"] = "You do not have a phone."
+}
