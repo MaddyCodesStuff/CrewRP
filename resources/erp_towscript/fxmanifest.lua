672b460e759853c34e3852bdb3e4095be4f704ce
tttony2x <tttony2x@gmail.com>
2021-10-24 03:40:34 -0400

    new tow script

---
+++ resources/erp_towscript/fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'adamant'
+
+author '! 1« NoobKeks_TV »#1337'
+description 'Top quality Tow-Scirpt'
+game 'gta5'
+
+client_script {
+    '@es_extended/locale.lua',
+    "client.lua",
+    "config.lua",
+    "locales/*.lua"
+}
+
+dependencies {
+'es_extended'
+
+}
